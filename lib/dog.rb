5a320b8d63a9468ff79b7baf0dac2e3ca5c87837
Tolase Durojaye <tolasesaheed@yahoo.co.uk>
2019-03-13 16:37:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,8 @@
 class Dog
-def initialize(name)
-  attr_accessor :name, breed
+def initialize(dogs_names)
+  attr_accessor :name, :breed
 end
 
 def dogs_name(name)
-  @name = name 
+  @name = name
 end
